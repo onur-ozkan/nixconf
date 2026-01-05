638f87a9803aa6695dd94c249c38dc7165300611
Onur Özkan <work@onurozkan.dev>
2026-01-05 23:07:06 +0300

    update orkavian devshell
    Signed-off-by: Onur Özkan <work@onurozkan.dev>

--- nixos/shells/orkavian.nix
+++ nixos/shells/orkavian.nix
@@ -9,6 +9,14 @@ pkgs.mkShell {
   ];
 
   buildInputs = with pkgs; [
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libxcb
+    xorg.libXi
+    libxkbcommon
+    vulkan-loader
+    mesa
+    libglvnd
     ffmpeg_6
     fontconfig
     llvmPackages.libclang
@@ -17,10 +25,21 @@ pkgs.mkShell {
     python3
   ];
 
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+    pkgs.libxkbcommon
+    pkgs.xorg.libX11
+    pkgs.xorg.libXcursor
+    pkgs.xorg.libxcb
+    pkgs.xorg.libXi
+    pkgs.vulkan-loader
+    pkgs.mesa
+    pkgs.libglvnd
+  ];
+
   RUST_LIB_SRC = pkgs.rustPlatform.rustLibSrc;
 
   shellHook = ''
-  	export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib"
+    export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib"
     export RUST_SRC_PATH="$RUST_LIB_SRC"
 
     alias make='make HOSTCC=gcc'
