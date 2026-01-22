c91b9e63261a30b802fbcff0d4691c6372b1fdd5
Onur Özkan <work@onurozkan.dev>
2026-01-22 12:29:35 +0300

    enable v4l2loopback in dual mode
    Signed-off-by: Onur Özkan <work@onurozkan.dev>

--- nixos/hosts/nimda/configuration.nix
+++ nixos/hosts/nimda/configuration.nix
@@ -30,8 +30,12 @@ with lib;
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
   boot.kernelModules = [ "v4l2loopback" ];
   boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=2 exclusive_caps=1
+  '';
 
   networking.hostName = "nimda";
   networking.networkmanager.enable = true;
