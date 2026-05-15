556b7b891591f64b26cad372f8a4cc5ed55afc5e
Onur Özkan <work@onurozkan.dev>
2026-05-15 07:47:28 +0300

    simplify trailing whitespace detection
    Signed-off-by: Onur Özkan <work@onurozkan.dev>

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,32 +1,8 @@
-local function clear_trailing_whitespace()
-    local match_id = vim.w.trailing_whitespace_match_id
-
-    if match_id then
-        pcall(vim.fn.matchdelete, match_id)
-        vim.w.trailing_whitespace_match_id = nil
-    end
-end
-
-local function highlight_trailing_whitespace()
-    clear_trailing_whitespace()
-    vim.w.trailing_whitespace_match_id = vim.fn.matchadd('Error', [[\s\+$]])
-end
+local setup_trailing_whitespace = require 'trailing_whitespace'
 
 local function setup()
-    -- Keep trailing space highlighting window-local.
     local trailing_whitespace_group = vim.api.nvim_create_augroup('TrailingWhitespace', { clear = true })
-
-    vim.api.nvim_create_autocmd('BufWinEnter', {
-        group = trailing_whitespace_group,
-        callback = highlight_trailing_whitespace,
-    })
-
-    vim.api.nvim_create_autocmd('BufWinLeave', {
-        group = trailing_whitespace_group,
-        callback = clear_trailing_whitespace,
-    })
-
-    highlight_trailing_whitespace()
+    setup_trailing_whitespace(trailing_whitespace_group)
 end
 
 return setup
---
+++ .config/nvim/lua/trailing_whitespace.lua
@@ -0,0 +1,28 @@
+local function set_highlight()
+    vim.api.nvim_set_hl(0, 'TrailingWhitespace', {
+        ctermbg = 1,
+        bg = '#992f33',
+    })
+end
+
+local function show()
+    vim.cmd([[match TrailingWhitespace /\s\+$/]])
+end
+
+local function setup(group)
+    set_highlight()
+
+    vim.api.nvim_create_autocmd('ColorScheme', {
+        group = group,
+        callback = set_highlight,
+    })
+
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'WinEnter', 'InsertLeave' }, {
+        group = group,
+        callback = show,
+    })
+
+    show()
+end
+
+return setup
